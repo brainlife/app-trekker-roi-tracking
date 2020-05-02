81d5c4d77f82e848407e2cac0c9263d08281ceef
Bradley Caron <bacaron@h1.carbonate.uits.iu.edu>
2020-05-01 21:09:30 -0400

    REason: compile mba

--- build.m
+++ build.m
@@ -2,6 +2,7 @@ addpath(genpath('/N/u/bacaron/git/wma_tools'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/u/brlife/git/encode'))
 addpath(genpath('/N/u/brlife/git/spm'))
+addpath(genpath('/N/u/brlife/git/mba'))
 addpath(genpath('/N/u/hayashis/git/vistasoft'))
 mcc -m -R -nodisplay -d compiled classificationGenerator
 mcc -m -R -nodisplay -d planeROI generatePlaneROI
