30d16701d700f1d18b47bd30185ba955d3edd8ed
Brad Caron <bacaron@iu.edu>
2020-05-03 15:50:11 -0400

    REason: remove 1000 streamline limit on visualization generator

--- classificationGenerator.m
+++ classificationGenerator.m
@@ -71,9 +71,9 @@ for it = 1:length(tracts)
 
    %tract.coords = tracts(it).fibers;
    %pick randomly up to 1000 fibers (pick all if there are less than 1000)
-   fiber_count = min(1000, numel(tracts{it}.fibers));
-   tract.coords = tracts{it}.fibers(randperm(fiber_count)); 
-   
+   %fiber_count = min(1000, numel(tracts{it}.fibers));
+   %tract.coords = tracts{it}.fibers(randperm(fiber_count)); 
+   tract.coords = tracts{it}.fibers; 
    savejson('', tract, fullfile('tracts',sprintf('%i.json',it)));
    all_tracts(it).filename = sprintf('%i.json',it);
    clear tract
