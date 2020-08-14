9f4404440da8dee7f5ba28f86e2a088c15ef2271
Bradley Caron <bacaron@i2.red.uits.iu.edu>
2020-08-14 19:35:39 -0400

    Reason: reduce cleaning

--- cleanFibers.m
+++ cleanFibers.m
@@ -2,11 +2,12 @@ function classification = cleanFibers(whole_classification,mergedFG)
 
 % generate new, empty classification structure
 classification = whole_classification;
-clean_sd = [4,3];
+%clean_sd = [4,3];
+clean_sd = 4;
 
 % remove streamlines based on cleaning criteria
-for ifg = 1:length(whole_classification.names)
-    for sd = 1:length(clean_sd)
-        classification = removeOutliersClassification(classification,mergedFG,clean_sd(sd),4,5);
-    end
-end
+%for ifg = 1:length(whole_classification.names)
+%    for sd = 1:length(clean_sd)
+classification = removeOutliersClassification(classification,mergedFG,clean_sd,4,5);
+%    end
+%end
