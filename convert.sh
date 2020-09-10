1dd254d5d1f1b726d5b6703e921b53da61c537d2
Brad Caron <bacaron@iu.edu>
2020-09-10 00:27:17 -0400

    reason: minor fixes

--- convert.sh
+++ convert.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
 
 roiPair=`jq -r '.roiPair' config.json`
+minDegree=`jq -r '.min_degree' config.json`
+maxDegree=`jq -r '.max_degree' config.json`
 
 pairs=($roiPair)
-range=` expr ${#pairs[@]}`
-nTracts=` expr ${range} / 2` 
+minDegree=($minDegree)
+maxDegree=($maxDegree)
+nTracts=` expr ${#pairs[@]}`
+nDegrees=` expr ${#minDegree[@]}`
 
-for i in ${!nTracts[@]}; do
-	holder=(*track$((i+1))*)
+for i in ${!pairs[@]}; do
+	for DEG in ${!minDegree[@]}; do
+		holder=(track$((i+1))_Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}*.vtk)
+		echo ${holder[*]}
 
-	for tractograms in ${holder[*]}; do
-		tckconvert ${tractograms} ${tractograms::-4}.tck
+		for tractograms in ${holder[*]}; do
+			tckconvert ${tractograms} ${tractograms::-4}.tck
+		done
+		
+		tcks=(track$((i+1))_Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}*.tck)
+		output=track$((i+1))_Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}.tck
+
+		if [ ${#tcks[@]} == 1 ]; then
+			mv ${tcks[0]} ${output}
+		else
+			tckedit ${tcks[*]} $output
+			mv ${tcks[*]} ./raw/
+		fi
+		tckinfo $output > track$((i+1))_Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}_info.txt
 	done
-	
-	tcks=(*track$((i+1))*.tck)
-	output=track$((i+1)).tck
-
-	if [ ${#tcks[@]} == 1 ]; then
-		mv ${tcks[0]} ${output}
-	else
-		tckedit ${tcks[*]} $output
-		mv ${tcks[*]} ./raw/
-	fi
-	tckinfo $output > track$((i+1))_info.txt
 done
 
-if [ -f track1.tck ]; then
-	mv *.mif *.vtk *.b* *.nii.gz ./raw/
+if [ -f track$((i+1))_Ecc${minDegree[$DEG]}to${maxDegree[$DEG]}.tck ]; then
+	mv *.mif *.vtk *.b* *.nii.gz *.func.gii ./raw/
 	holder=(track*.tck)
 	if [ ${#holder[@]} == 1 ]; then
         cp -v ${holder[0]} ./track/track.tck
@@ -36,5 +43,5 @@ if [ -f track1.tck ]; then
 	tckinfo ./track/track.tck > ./track/track_info.txt
 else
     echo "tracking did not generate. please check derivatives and log files for debugging"
-    exit 1
+    #exit 1
 fi	
